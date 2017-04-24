5d76d253fb8e764560e0bb157e2d176201268206
Rana Das <rana.pratap.das@gmail.com>
2017-04-24 08:41:30 +0100

    formatting

--- Chapter05/ErrorHelperTypeScript.ts
+++ Chapter05/ErrorHelperTypeScript.ts
@@ -1,20 +1,20 @@
-
+'use strict';
 
 window.onload = () => {
     var failureMessage = {
-        responseText : {
-            "failure" : "true",
-            "errorMessage" : "Error Message from Typescript"
+        responseText: {
+            "failure": "true",
+            "errorMessage": "Error Message from Typescript"
         }
-    }    
-    
+    };
+
     if (ErrorHelper.containsErrors(failureMessage))
         ErrorHelper.trace(failureMessage);
-        
+
     // var anotherFailure: IResponse = {
     //     responseText: {
     //         success: true   
     //     }
     // }
-}
+};
 
